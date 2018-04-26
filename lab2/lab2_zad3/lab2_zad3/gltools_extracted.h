605ce317052998b1430985a2594af205926fc067
Szymon Dyrcz <szymon.dd@outlook.com>
2018-04-26 16:37:01 +0200

    Revert "lab2"
    This reverts commit 880093177d7f8ebaf63fefcf1414ab9b71f9e944.

--- lab2/lab2_zad3/lab2_zad3/gltools_extracted.h
+++
@@ -1,5 +0,0 @@
-// Some data types
-typedef GLfloat GLTVector2[2];      // Two component floating point vector
-typedef GLfloat GLTVector3[3];      // Three component floating point vector
-typedef GLfloat GLTVector4[4];      // Four component floating point vector
-typedef GLfloat GLTMatrix[16];      // A column major 4x4 matrix of type GLfloat
