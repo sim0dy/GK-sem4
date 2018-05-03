5f742acd1b2bf0fadb01072006bb859055a2f8ee
Szymon Dyrcz <szymon.dd@outlook.com>
2018-05-03 16:10:43 +0200

    lab4

---
+++ lab4/lab4_zad2/lab4_zad2/colors.h
@@ -0,0 +1,855 @@
+/*
+(c) Janusz Ganczarski
+http://www.januszg.hg.pl
+JanuszG@enter.net.pl
+*/
+
+#ifndef __COLORS__H__
+#define __COLORS__H__
+
+#include <GL/gl.h>
+
+// definicje kolorów RGB w ASP.NET (System.Drawing.Color)
+// Ÿród³o:
+// http://www.opinionatedgeek.com/DotNet/Tools/Colors/default.aspx
+
+
+const GLdouble AliceBlue [4] =
+    {
+        0.941176, 0.972549, 1.000000, 1.000000
+    };
+
+const GLdouble AntiqueWhite [4] =
+    {
+        0.980392, 0.921569, 0.843137, 1.000000
+    };
+
+const GLdouble Aqua [4] =
+    {
+        0.000000, 1.000000, 1.000000, 1.000000
+    };
+
+const GLdouble Aquamarine [4] =
+    {
+        0.498039, 1.000000, 0.831373, 1.000000
+    };
+
+// lazurowy
+
+const GLdouble Azure [4] =
+    {
+        0.941176, 1.000000, 1.000000, 1.000000
+    };
+
+// be¿owy
+
+const GLdouble Beige [4] =
+    {
+        0.960784, 0.960784, 0.862745, 1.000000
+    };
+
+const GLdouble Bisque [4] =
+    {
+        1.000000, 0.894118, 0.768627, 1.000000
+    };
+
+// czarny
+
+const GLfloat Black [4] =
+    {
+        0.000000, 0.000000, 0.000000, 1.000000
+    };
+
+const GLdouble BlanchedAlmond [4] =
+    {
+        1.000000, 0.921569, 0.803922, 1.000000
+    };
+
+// niebieski
+
+const GLdouble Blue [4] =
+    {
+        0.000000, 0.000000, 1.000000, 1.000000
+    };
+
+// niebieskofiletowy
+
+const GLdouble BlueViolet [4] =
+    {
+        0.541176, 0.168627, 0.886275, 1.000000
+    };
+
+// br¹zowy
+
+const GLdouble Brown [4] =
+    {
+        0.647059, 0.164706, 0.164706, 1.000000
+    };
+
+const GLdouble BurlyWood [4] =
+    {
+        0.870588, 0.721569, 0.529412, 1.000000
+    };
+
+const GLdouble CadetBlue [4] =
+    {
+        0.372549, 0.619608, 0.627451, 1.000000
+    };
+
+const GLdouble Chartreuse [4] =
+    {
+        0.498039, 1.000000, 0.000000, 1.000000
+    };
+
+// czekoladowy
+
+const GLdouble Chocolate [4] =
+    {
+        0.823529, 0.411765, 0.117647, 1.000000
+    };
+
+// koralowy
+
+const GLdouble Coral [4] =
+    {
+        1.000000, 0.498039, 0.313725, 1.000000
+    };
+
+const GLdouble CornflowerBlue [4] =
+    {
+        0.392157, 0.584314, 0.929412, 1.000000
+    };
+
+const GLdouble Cornsilk [4] =
+    {
+        1.000000, 0.972549, 0.862745, 1.000000
+    };
+
+// karmazynowy (purpurowy)
+
+const GLdouble Crimson [4] =
+    {
+        0.862745, 0.078431, 0.235294, 1.000000
+    };
+
+// niebieskozielony
+
+const GLdouble Cyan [4] =
+    {
+        0.000000, 1.000000, 1.000000, 1.000000
+    };
+
+// ciemnoniebieski
+
+const GLdouble DarkBlue [4] =
+    {
+        0.000000, 0.000000, 0.545098, 1.000000
+    };
+
+// ciemnoniebieskozielony
+
+const GLdouble DarkCyan [4] =
+    {
+        0.000000, 0.545098, 0.545098, 1.000000
+    };
+
+const GLdouble DarkGoldenrod [4] =
+    {
+        0.721569, 0.525490, 0.043137, 1.000000
+    };
+
+// ciemnoszary
+
+const GLdouble DarkGray [4] =
+    {
+        0.662745, 0.662745, 0.662745, 1.000000
+    };
+
+// ciemnozielony
+
+const GLdouble DarkGreen [4] =
+    {
+        0.000000, 0.392157, 0.000000, 1.000000
+    };
+
+// ciemny khaki
+
+const GLdouble DarkKhaki [4] =
+    {
+        0.741176, 0.717647, 0.419608, 1.000000
+    };
+
+// ciemny purpurowy
+
+const GLdouble DarkMagenta [4] =
+    {
+        0.545098, 0.000000, 0.545098, 1.000000
+    };
+
+const GLdouble DarkOliveGreen [4] =
+    {
+        0.333333, 0.419608, 0.184314, 1.000000
+    };
+
+// ciemnopomarañczowy
+
+const GLdouble DarkOrange [4] =
+    {
+        1.000000, 0.549020, 0.000000, 1.000000
+    };
+
+const GLdouble DarkOrchid [4] =
+    {
+        0.600000, 0.196078, 0.800000, 1.000000
+    };
+
+// ciemnoczerwony
+
+const GLdouble DarkRed [4] =
+    {
+        0.545098, 0.000000, 0.000000, 1.000000
+    };
+
+const GLdouble DarkSalmon [4] =
+    {
+        0.913725, 0.588235, 0.478431, 1.000000
+    };
+
+const GLdouble DarkSeaGreen [4] =
+    {
+        0.560784, 0.737255, 0.545098, 1.000000
+    };
+
+const GLdouble DarkSlateBlue [4] =
+    {
+        0.282353, 0.239216, 0.545098, 1.000000
+    };
+
+const GLdouble DarkSlateGray [4] =
+    {
+        0.184314, 0.309804, 0.309804, 1.000000
+    };
+
+const GLdouble DarkTurquoise [4] =
+    {
+        0.000000, 0.807843, 0.819608, 1.000000
+    };
+
+// ciemnofioletowy
+
+const GLdouble DarkViolet [4] =
+    {
+        0.580392, 0.000000, 0.827451, 1.000000
+    };
+
+const GLdouble DeepPink [4] =
+    {
+        1.000000, 0.078431, 0.576471, 1.000000
+    };
+
+const GLdouble DeepSkyBlue [4] =
+    {
+        0.000000, 0.749020, 1.000000, 1.000000
+    };
+
+const GLdouble DimGray [4] =
+    {
+        0.411765, 0.411765, 0.411765, 1.000000
+    };
+
+const GLdouble DodgerBlue [4] =
+    {
+        0.117647, 0.564706, 1.000000, 1.000000
+    };
+
+const GLdouble Firebrick [4] =
+    {
+        0.698039, 0.133333, 0.133333, 1.000000
+    };
+
+const GLdouble FloralWhite [4] =
+    {
+        1.000000, 0.980392, 0.941176, 1.000000
+    };
+
+const GLdouble ForestGreen [4] =
+    {
+        0.133333, 0.545098, 0.133333, 1.000000
+    };
+
+// fuksja
+
+const GLdouble Fuchsia [4] =
+    {
+        1.000000, 0.000000, 1.000000, 1.000000
+    };
+
+const GLdouble Gainsboro [4] =
+    {
+        0.862745, 0.862745, 0.862745, 1.000000
+    };
+
+const GLdouble GhostWhite [4] =
+    {
+        0.972549, 0.972549, 1.000000, 1.000000
+    };
+
+// z³oty
+
+const GLdouble Gold [4] =
+    {
+        1.000000, 0.843137, 0.000000, 1.000000
+    };
+
+const GLdouble Goldenrod [4] =
+    {
+        0.854902, 0.647059, 0.125490, 1.000000
+    };
+
+// szary
+
+const GLdouble Gray [4] =
+    {
+        0.501961, 0.501961, 0.501961, 1.000000
+    };
+
+// zielony
+
+const GLdouble Green [4] =
+    {
+        0.000000, 0.501961, 0.000000, 1.000000
+    };
+
+// zielono¿ó³ty
+
+const GLdouble GreenYellow [4] =
+    {
+        0.678431, 1.000000, 0.184314, 1.000000
+    };
+
+// spadziowy
+
+const GLdouble Honeydew [4] =
+    {
+        0.941176, 1.000000, 0.941176, 1.000000
+    };
+
+// intensywny ró¿owy
+
+const GLdouble HotPink [4] =
+    {
+        1.000000, 0.411765, 0.705882, 1.000000
+    };
+
+const GLdouble IndianRed [4] =
+    {
+        0.803922, 0.360784, 0.360784, 1.000000
+    };
+
+// indygo
+
+const GLdouble Indigo [4] =
+    {
+        0.294118, 0.000000, 0.509804, 1.000000
+    };
+
+// koœæ s³oniowa
+
+const GLdouble Ivory [4] =
+    {
+        1.000000, 1.000000, 0.941176, 1.000000
+    };
+
+// khaki
+
+const GLdouble Khaki [4] =
+    {
+        0.941176, 0.901961, 0.549020, 1.000000
+    };
+
+// lawendowy
+
+const GLdouble Lavender [4] =
+    {
+        0.901961, 0.901961, 0.980392, 1.000000
+    };
+
+const GLdouble LavenderBlush [4] =
+    {
+        1.000000, 0.941176, 0.960784, 1.000000
+    };
+
+const GLdouble LawnGreen [4] =
+    {
+        0.486275, 0.988235, 0.000000, 1.000000
+    };
+
+const GLdouble LemonChiffon [4] =
+    {
+        1.000000, 0.980392, 0.803922, 1.000000
+    };
+
+const GLdouble LightBlue [4] =
+    {
+        0.678431, 0.847059, 0.901961, 1.000000
+    };
+
+const GLdouble LightCoral [4] =
+    {
+        0.941176, 0.501961, 0.501961, 1.000000
+    };
+
+const GLdouble LightCyan [4] =
+    {
+        0.878431, 1.000000, 1.000000, 1.000000
+    };
+
+const GLdouble LightGoldenrodYellow [4] =
+    {
+        0.980392, 0.980392, 0.823529, 1.000000
+    };
+
+const GLdouble LightGray [4] =
+    {
+        0.827451, 0.827451, 0.827451, 1.000000
+    };
+
+const GLdouble LightGreen [4] =
+    {
+        0.564706, 0.933333, 0.564706, 1.000000
+    };
+
+const GLdouble LightPink [4] =
+    {
+        1.000000, 0.713725, 0.756863, 1.000000
+    };
+
+const GLdouble LightSalmon [4] =
+    {
+        1.000000, 0.627451, 0.478431, 1.000000
+    };
+
+const GLdouble LightSeaGreen [4] =
+    {
+        0.125490, 0.698039, 0.666667, 1.000000
+    };
+
+const GLdouble LightSkyBlue [4] =
+    {
+        0.529412, 0.807843, 0.980392, 1.000000
+    };
+
+const GLdouble LightSlateGray [4] =
+    {
+        0.466667, 0.533333, 0.600000, 1.000000
+    };
+
+const GLdouble LightSteelBlue [4] =
+    {
+        0.690196, 0.768627, 0.870588, 1.000000
+    };
+
+const GLdouble LightYellow [4] =
+    {
+        1.000000, 1.000000, 0.878431, 1.000000
+    };
+
+// cytrynowy
+
+const GLdouble Lime [4] =
+    {
+        0.000000, 1.000000, 0.000000, 1.000000
+    };
+
+// cytrynowozielony
+
+const GLdouble LimeGreen [4] =
+    {
+        0.196078, 0.803922, 0.196078, 1.000000
+    };
+
+// lniany
+
+const GLdouble Linen [4] =
+    {
+        0.980392, 0.941176, 0.901961, 1.000000
+    };
+
+// purpurowy
+
+const GLdouble Magenta [4] =
+    {
+        1.000000, 0.000000, 1.000000, 1.000000
+    };
+
+// kasztanowy, br¹zowoczerwony
+
+const GLdouble Maroon [4] =
+    {
+        0.501961, 0.000000, 0.000000, 1.000000
+    };
+
+const GLdouble MediumAquamarine [4] =
+    {
+        0.400000, 0.803922, 0.666667, 1.000000
+    };
+
+const GLdouble MediumBlue [4] =
+    {
+        0.000000, 0.000000, 0.803922, 1.000000
+    };
+
+const GLdouble MediumOrchid [4] =
+    {
+        0.729412, 0.333333, 0.827451, 1.000000
+    };
+
+const GLdouble MediumPurple [4] =
+    {
+        0.576471, 0.439216, 0.858824, 1.000000
+    };
+
+const GLdouble MediumSeaGreen [4] =
+    {
+        0.235294, 0.701961, 0.443137, 1.000000
+    };
+
+const GLdouble MediumSlateBlue [4] =
+    {
+        0.482353, 0.407843, 0.933333, 1.000000
+    };
+
+const GLdouble MediumSpringGreen [4] =
+    {
+        0.000000, 0.980392, 0.603922, 1.000000
+    };
+
+const GLdouble MediumTurquoise [4] =
+    {
+        0.282353, 0.819608, 0.800000, 1.000000
+    };
+
+const GLdouble MediumVioletRed [4] =
+    {
+        0.780392, 0.082353, 0.521569, 1.000000
+    };
+
+const GLdouble MidnightBlue [4] =
+    {
+        0.098039, 0.098039, 0.439216, 1.000000
+    };
+
+const GLdouble MintCream [4] =
+    {
+        0.960784, 1.000000, 0.980392, 1.000000
+    };
+
+const GLdouble MistyRose [4] =
+    {
+        1.000000, 0.894118, 0.882353, 1.000000
+    };
+
+const GLdouble Moccasin [4] =
+    {
+        1.000000, 0.894118, 0.709804, 1.000000
+    };
+
+const GLdouble NavajoWhite [4] =
+    {
+        1.000000, 0.870588, 0.678431, 1.000000
+    };
+
+// granatowy
+
+const GLdouble Navy [4] =
+    {
+        0.000000, 0.000000, 0.501961, 1.000000
+    };
+
+const GLdouble OldLace [4] =
+    {
+        0.992157, 0.960784, 0.901961, 1.000000
+    };
+
+// oliwkowy
+
+const GLdouble Olive [4] =
+    {
+        0.501961, 0.501961, 0.000000, 1.000000
+    };
+
+// khaki, szarozielony
+
+
+const GLdouble OliveDrab [4] =
+    {
+        0.419608, 0.556863, 0.137255, 1.000000
+    };
+
+// oran¿, pomarañczowy
+
+const GLdouble Orange [4] =
+    {
+        1.000000, 0.647059, 0.000000, 1.000000
+    };
+
+// pomarañczowoczerwony
+
+const GLdouble OrangeRed [4] =
+    {
+        1.000000, 0.270588, 0.000000, 1.000000
+    };
+
+const GLdouble Orchid [4] =
+    {
+        0.854902, 0.439216, 0.839216, 1.000000
+    };
+
+const GLdouble PaleGoldenrod [4] =
+    {
+        0.933333, 0.909804, 0.666667, 1.000000
+    };
+
+// bladozielony
+
+const GLdouble PaleGreen [4] =
+    {
+        0.596078, 0.984314, 0.596078, 1.000000
+    };
+
+// bladoturkusowy
+
+const GLdouble PaleTurquoise [4] =
+    {
+        0.686275, 0.933333, 0.933333, 1.000000
+    };
+
+const GLdouble PaleVioletRed [4] =
+    {
+        0.858824, 0.439216, 0.576471, 1.000000
+    };
+
+const GLdouble PapayaWhip [4] =
+    {
+        1.000000, 0.937255, 0.835294, 1.000000
+    };
+
+const GLdouble PeachPuff [4] =
+    {
+        1.000000, 0.854902, 0.725490, 1.000000
+    };
+
+const GLdouble Peru [4] =
+    {
+        0.803922, 0.521569, 0.247059, 1.000000
+    };
+
+// ró¿owy
+
+const GLdouble Pink [4] =
+    {
+        1.000000, 0.752941, 0.796078, 1.000000
+    };
+
+// œliwkowy
+
+const GLdouble Plum [4] =
+    {
+        0.866667, 0.627451, 0.866667, 1.000000
+    };
+
+const GLdouble PowderBlue [4] =
+    {
+        0.690196, 0.878431, 0.901961, 1.000000
+    };
+
+// purpurowy
+
+const GLdouble Purple [4] =
+    {
+        0.501961, 0.000000, 0.501961, 1.000000
+    };
+
+// czerwony
+
+const GLdouble Red [4] =
+    {
+        1.000000, 0.000000, 0.000000, 1.000000
+    };
+
+const GLdouble RosyBrown [4] =
+    {
+        0.737255, 0.560784, 0.560784, 1.000000
+    };
+
+const GLdouble RoyalBlue [4] =
+    {
+        0.254902, 0.411765, 0.882353, 1.000000
+    };
+
+// brunatny
+
+const GLdouble SaddleBrown [4] =
+    {
+        0.545098, 0.270588, 0.074510, 1.000000
+    };
+
+// ³ososiowy
+
+const GLdouble Salmon [4] =
+    {
+        0.980392, 0.501961, 0.447059, 1.000000
+    };
+
+const GLdouble SandyBrown [4] =
+    {
+        0.956863, 0.643137, 0.376471, 1.000000
+    };
+
+const GLdouble SeaGreen [4] =
+    {
+        0.180392, 0.545098, 0.341176, 1.000000
+    };
+
+const GLdouble SeaShell [4] =
+    {
+        1.000000, 0.960784, 0.933333, 1.000000
+    };
+
+// sjena
+
+const GLdouble Sienna [4] =
+    {
+        0.627451, 0.321569, 0.176471, 1.000000
+    };
+
+// srebrny
+
+const GLdouble Silver [4] =
+    {
+        0.752941, 0.752941, 0.752941, 1.000000
+    };
+
+// b³êkitny, jasnoniebieski, lazurowy
+
+const GLdouble SkyBlue [4] =
+    {
+        0.529412, 0.807843, 0.921569, 1.000000
+    };
+
+// ³upkowoniebieski
+
+const GLdouble SlateBlue [4] =
+    {
+        0.415686, 0.352941, 0.803922, 1.000000
+    };
+
+// ³upkowoszary
+
+const GLdouble SlateGray [4] =
+    {
+        0.439216, 0.501961, 0.564706, 1.000000
+    };
+
+// œnie¿ny
+
+const GLdouble Snow [4] =
+    {
+        1.000000, 0.980392, 0.980392, 1.000000
+    };
+
+// zielony wiosenny
+
+const GLdouble SpringGreen [4] =
+    {
+        0.000000, 1.000000, 0.498039, 1.000000
+    };
+
+// stalowoniebieski, szaroniebieski
+
+const GLdouble SteelBlue [4] =
+    {
+        0.274510, 0.509804, 0.705882, 1.000000
+    };
+
+// jasnobr¹zowy
+
+const GLdouble Tan [4] =
+    {
+        0.823529, 0.705882, 0.549020, 1.000000
+    };
+
+// cyrankowy
+
+const GLdouble Teal [4] =
+    {
+        0.000000, 0.501961, 0.501961, 1.000000
+    };
+
+// ostowy
+
+const GLdouble Thistle [4] =
+    {
+        0.847059, 0.749020, 0.847059, 1.000000
+    };
+
+// pomidorowy
+
+const GLdouble Tomato [4] =
+    {
+        1.000000, 0.388235, 0.278431, 1.000000
+    };
+
+// przezroczysty
+
+const GLdouble Transparent [4] =
+    {
+        1.000000, 1.000000, 1.000000, 1.000000
+    };
+
+// turkusowy
+
+const GLdouble Turquoise [4] =
+    {
+        0.250980, 0.878431, 0.815686, 1.000000
+    };
+
+// fioletowy
+
+const GLdouble Violet [4] =
+    {
+        0.933333, 0.509804, 0.933333, 1.000000
+    };
+
+const GLdouble Wheat [4] =
+    {
+        0.960784, 0.870588, 0.701961, 1.000000
+    };
+
+// bia³y
+
+const GLdouble White [4] =
+    {
+        1.000000, 1.000000, 1.000000, 1.000000
+    };
+
+const GLdouble WhiteSmoke [4] =
+    {
+        0.960784, 0.960784, 0.960784, 1.000000
+    };
+
+// Ÿó³ty
+
+const GLdouble Yellow [4] =
+    {
+        1.000000, 1.000000, 0.000000, 1.000000
+    };
+
+// ¿ó³tozielony
+
+const GLdouble YellowGreen [4] =
+    {
+        0.603922, 0.803922, 0.196078, 1.000000
+    };
+
+#endif // __COLORS_H__
